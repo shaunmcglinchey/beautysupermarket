afea3115b9d5cb65974e8d6861a4f7a3a20ff990
shaunmcglinchey <mcglincs@cs.man.ac.uk>
2014-12-19 15:55:23 -0200

    added ui clear filters

--- server.js
+++ server.js
@@ -389,7 +389,7 @@ function removeUndesiredCategories(result){
             return category.id == 14090 || category.id == 14029 || category.id == 32538 || category.id == 14091 || category.id == 1 || category.id == 31000
                  || category.id == 27000 || category.id == 23000 || category.id == 21000 || category.id == 12000 || category.id == 16000 || category.id == 32194
                 || category.id == 7000 || category.id == 15000 || category.id == 3000 || category.id == 10000 || category.id == 32346 || category.id == 9000 || category.id == 2000
-                || category.id == 5000 || category.id == 9000 || category.id == 11000 || category.id == 22000 || category.id == 9001
+                || category.id == 5000 || category.id == 9000 || category.id == 11000 || category.id == 22000 || category.id == 9001 || category.id == 9100
                 || category.id == 8400 || category.id == 24000;
         });
     }
